689e587670d37afdf7df0eaa13a528f9890123d8
IvoDabove <ivomartindabove96@gmail.com>
2025-08-11 19:33:34 -0300

    Change in Reqest Career

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/api/v1/', GeneralRouter); 
-app.use('/career', CareerRouter); // Ensure the path is correct
+app.use('/Career', CareerRouter); // Ensure the path is correct
 
 
 
