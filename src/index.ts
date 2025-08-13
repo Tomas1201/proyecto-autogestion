2ff1c7095f9debedb85c3d15e1ab614fe538f75e
IvoDabove <ivomartindabove96@gmail.com>
2025-08-13 18:56:33 -0300

    Index Changes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import a from './Shared/Relaciones.js'; 
 import express from "express";
 import GeneralRouter from "./Features/GeneralRouter.js"
-import CareerRouter from './Features/CareerCRUD/CareerRouter.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 a;
 app.use('/api/v1/', GeneralRouter); 
-app.use('/Career', CareerRouter); 
+
 
 
 
