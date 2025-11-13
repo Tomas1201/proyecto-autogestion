91d68dd6f8fbe3750c247da66d367dbd5ab435ef
IvoDabove <ivomartindabove96@gmail.com>
2025-11-13 16:52:27 -0300

    subject-changes

--- src/features/general.router.ts
+++ src/features/general.router.ts
@@ -3,16 +3,10 @@ import {CareerRouter} from "./career/career-crud/career-crud.router.js";
 import CareerSearchRouter from "./career/career-search/career-search.router.js";
 import {studentCRUDRouter} from './student/student-crud/student-crud.router.js';
 import {StudentSearchRouter} from './student/student-search/student-search.router.js';
-<<<<<<< HEAD
-import { ProfessorSearchRouter } from "./Professor/ProfessorSearch/ProfessorSearchRouter.js";
-import {ProfessorCRUDRouter} from "./Professor/ProfessorCRUD/ProfessorCRUDRouter.js";
-import { SubjectRouter } from "./SubjectCRUD/SubjectRouter.js";
-=======
 import { ProfessorSearchRouter } from "./professor/professor-search/professor-search.router.js";
 import {ProfessorCRUDRouter} from "./professor/professor-crud/professor-crud.router.js";
 import {authRouter} from "./auth/auth.router.js";
->>>>>>> a5f67a278438368304d109671c21ea4a752b0b7f
-
+import {SubjectRouter} from "./SubjectCRUD/SubjectRouter.js";
  const generalRouter = Router();
 generalRouter.use("/auth", authRouter)
 generalRouter.use("/career", CareerRouter);
